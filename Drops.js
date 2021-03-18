8b668747531d3337a1069923742f0478fd98be95
NAINIKAGOSWAMI <74713715+NAINIKAGOSWAMI@users.noreply.github.com>
2021-03-18 21:23:09 +0530

    Add files via upload

--- Drops.js
+++ Drops.js
@@ -4,9 +4,16 @@ class Drops {
             friction:0.1,
             restitution:0.1
 }
- }
-    var maxDrops=100;
-    for(var i = 0; i<maxDrops;i++){
-        drops.push(new createDrop(random(0,400),random(0,400)));
+ 
+ this.rain = Bodies.circle(x,y,5,options)
+ this.radius = 5;
+ World.add(world,this.rain);
+}   
+
+updateY(){     
+    if(this.rain.position.y > height){
+
+        Matter.Body.setPosition(this.rain, {x:random(0,400), y:random(0,400)})
     }
+}
 }
--- sketch.js
+++ sketch.js
@@ -4,7 +4,9 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var drops,maxDrops;
+var engine,world;
+var drops=[];
+var maxDrops = 100;
 
 function preload()
 {
@@ -12,25 +14,33 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
-
-
 	engine = Engine.create();
 	world = engine.world;
+	createCanvas(800, 700);
 
-	drops = new Drops(400,300);
+	// drops = new Drops(400,300);
 
-	//Create the Bodies Here.
+	// var maxDrops=100;
 
-
-	Engine.run(engine);
+  if (frameCount % 150===0){
+    for(var i = 0; i<maxDrops;i++){
+        drops.push(new createDrop(random(0,400),random(0,400)));
+    }
+  }
   
 }
 
 
 function draw() {
+  Engine.update(engine);
   rectMode(CENTER);
   background(0);
+
+  //displaying rain drops
+  for(var i = 0; i<maxDrops; i++){
+    drops[i].showDrop();
+    drops[i].updateY()
+  }
 
   drops.display();
   
